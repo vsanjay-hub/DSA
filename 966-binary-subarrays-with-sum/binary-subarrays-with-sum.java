f84e338b7bcafed652f5d011925970904e5366b3
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2025-09-01 21:47:00 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (85.01%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;  
+
+        int l = 0, sum = 0, count = 0;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum > goal) {
+                sum -= nums[l++];
+            }
+            count += (r - l + 1);
+        }
+        return count;
+    }
+}
