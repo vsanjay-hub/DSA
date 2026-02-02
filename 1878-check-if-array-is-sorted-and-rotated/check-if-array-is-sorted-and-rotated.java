927bd9f3ab9d9528bf00a52b620661f6bf7c5d52
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-02-02 19:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (8.99%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
+        }
+        if (nums[0] < nums[n - 1]) {
+            count++;
+        }
+
+        return count <= 1;
+    }
+}
