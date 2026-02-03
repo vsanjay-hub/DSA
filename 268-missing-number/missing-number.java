b55b354b8de5674fa977cc0f2d7f296e3a44af3f
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-02-03 22:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (18.16%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor;
+    }
+}
