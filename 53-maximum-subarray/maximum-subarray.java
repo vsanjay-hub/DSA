3c9070fee7133ecf5c22fce6f6d484b6731dfc03
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-02-20 17:47:03 +0530

    Time: 1 ms (99.68%) | Memory: 77.4 MB (47.22%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max=Math.max(max,sum);
+            if(sum<0){
+                sum=0;
+            }
+              
+        }
+     return max;
+
+    }
+}
