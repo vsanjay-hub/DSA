f480118cb079710b57623ae35eb67d49e59ef52b
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-02-28 22:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (74.54%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,16 +1,19 @@
 class Solution {
     public int findMin(int[] nums) {
-        int left =0;
-        int right=nums.length-1;
-        while(left<right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]>nums[right]){
-                left=mid+1;
+        int low=0;
+        int min = Integer.MAX_VALUE;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[low]<=nums[mid]){
+        min=Math.min(min,nums[low]);
+        low=mid+1;
             }
             else{
-                right=mid;
+                min=Math.min(min,nums[mid]);
+                high=mid-1;
             }
         }
-        return nums[left];
+        return min;
     }
 }
