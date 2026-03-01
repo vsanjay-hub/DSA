f7c9e714eae39b86ef9f43b122706c59cbae5a0b
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-03-01 15:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (90.90%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,19 +1,25 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int start=0;
-        int end=nums.length-1;
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if (mid%2==1){
-                mid--;
+          int low=0;
+          int high=nums.length;;
+          int n=nums.length;
+          if(n==1){
+            return nums[0];
+          }
+          if(nums[0]!=nums[1])return nums[0];
+          if(nums[n-1]!=nums[n-2])return nums[n-1];
+          while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid+1]&&nums[mid]!=nums[mid-1]){
+                return nums[mid];
             }
-            if(nums[mid]==nums[mid+1]){
-                start=mid+2;
+            if((nums[mid]==nums[mid-1]&&mid%2==1)||(nums[mid]==nums[mid+1]&&mid%2==0)){
+                low=mid+1;
             }
             else{
-                end=mid;
+                high=mid-1;
             }
-        }
-        return nums[start];
+          }
+          return -1;
     }
 }
