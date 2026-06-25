69bbd1e168a9bc9518dce9f45b3e25aa80445332
VEDULLAPALLI MANI SAI SANJAY <23B91A05V1@srkrec.ac.in>
2026-06-25 20:53:45 +0530

    Time: 10 ms (78.53%) | Memory: 48.6 MB (46.58%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < 2 * n; i++) {
+
+            while (!stack.isEmpty() &&
+                   nums[i % n] > nums[stack.peek()]) {
+
+                res[stack.pop()] = nums[i % n];
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return res;
+    }
+}
